b63c96ed4f19354a50f83d690b2fa8f2697a4518
Pranta Saha <prantasaha907@gmail.com>
2025-05-11 12:29:34 +0600

    User Entity

--- LibraryMangement/src/main/java/com/pranta/LibraryMangement/Entity/User.java
+++ LibraryMangement/src/main/java/com/pranta/LibraryMangement/Entity/User.java
@@ -24,15 +24,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 @Table(name = "users")
-public class UserEntity {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private String name;
+
     @Column(unique = true, nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private UserRole role;
