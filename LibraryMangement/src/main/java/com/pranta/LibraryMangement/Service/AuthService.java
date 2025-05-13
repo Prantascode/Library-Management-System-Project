3e1169071a43d856b2570e840e8eb01b149e2262
Pranta Saha <prantasaha907@gmail.com>
2025-05-13 18:24:45 +0600

    13/5/25

---
+++ LibraryMangement/src/main/java/com/pranta/LibraryMangement/Service/AuthService.java
@@ -0,0 +1,11 @@
+package com.pranta.LibraryMangement.Service;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    
+    // private final AuthenticationManager authenticationManager;
+    // private final Userser
+}
---
+++ LibraryMangement/src/main/java/com/pranta/LibraryMangement/Service/CustomUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.pranta.LibraryMangement.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.pranta.LibraryMangement.Entity.User;
+import com.pranta.LibraryMangement.Repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found with email"+email));
+
+        return new org.springframework.security.core.userdetails.User(
+            user.getEmail(),
+            user.getPassword(),
+            Collections.singletonList(new SimpleGrantedAuthority("Role_" + user.getRole().name()))
+        );
+    }
+    
+}
